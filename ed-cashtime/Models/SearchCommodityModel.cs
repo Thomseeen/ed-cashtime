799d791e9a54b2dd8b9b3137f84cae5503a4adae
thomseeen <thwa1@web.de>
2021-05-25 23:02:26 +0200

    structure

---
+++ ed-cashtime/Models/SearchCommodityModel.cs
@@ -0,0 +1,15 @@
+﻿namespace EdCashtime.Models {
+    public class SearchCommodityModel {
+        public string Name { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public long? MinBuyPrice { get; set; }
+
+        public long? MinBuyDemand { get; set; }
+
+        public long? MaxSellPrice { get; set; }
+
+        public long? MinStock { get; set; }
+    }
+}
